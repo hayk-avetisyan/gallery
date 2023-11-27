e829f1178eb27ab5be24c87c2d4441fed1759df8
Hayk Avetisyan <hayk.avetisyan@synisys.com>
2023-11-27 17:12:20 +0400

    Other sample endpoints

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,11 +5,12 @@ import axios from 'axios';
 @Controller()
 export class AppController {
   
-  private static readonly side: number = 50;
+  private static readonly side: number = 400;
+  private static readonly url: string = 'https://images.pexels.com/photos/378570/pexels-photo-378570.jpeg?auto=compress&cs=tinysrgb&w=100000';
 
   @Get("/contain")
   contain(@Res() response): void {
-    axios({url: 'https://images.pexels.com/photos/378570/pexels-photo-378570.jpeg?auto=compress&cs=tinysrgb&w=100000', responseType: 'stream'})
+    axios({url: AppController.url, responseType: 'stream'})
         .then(response => response.data.pipe(sharp()))
         .then(source => source.resize(AppController.side, AppController.side, {fit: sharp.fit.contain}))
         .then(file => file.pipe(response))
@@ -17,7 +18,7 @@ export class AppController {
 
   @Get("/fill")
   fill(@Res() response): void {
-    axios({url: 'https://images.pexels.com/photos/378570/pexels-photo-378570.jpeg?auto=compress&cs=tinysrgb&w=100000', responseType: 'stream'})
+    axios({url: AppController.url, responseType: 'stream'})
         .then(response => response.data.pipe(sharp()))
         .then(source => source.resize(AppController.side, AppController.side, {fit: sharp.fit.fill}))
         .then(file => file.pipe(response))
@@ -26,7 +27,7 @@ export class AppController {
 
   @Get("/cover")
   cover(@Res() response): void {
-    axios({url: 'https://images.pexels.com/photos/378570/pexels-photo-378570.jpeg?auto=compress&cs=tinysrgb&w=100000', responseType: 'stream'})
+    axios({url: AppController.url, responseType: 'stream'})
         .then(response => response.data.pipe(sharp()))
         .then(source => source.resize(AppController.side, AppController.side, {fit: sharp.fit.cover}))
         .then(file => file.pipe(response))
@@ -34,7 +35,7 @@ export class AppController {
 
   @Get("/inside")
   inside(@Res() response): void {
-    axios({url: 'https://images.pexels.com/photos/378570/pexels-photo-378570.jpeg?auto=compress&cs=tinysrgb&w=100000', responseType: 'stream'})
+    axios({url: AppController.url, responseType: 'stream'})
         .then(response => response.data.pipe(sharp()))
         .then(source => source.resize(AppController.side, AppController.side, {fit: sharp.fit.inside}))
         .then(file => file.pipe(response))
@@ -42,9 +43,53 @@ export class AppController {
 
   @Get("/outside")
   outside(@Res() response): void {
-    axios({url: 'https://images.pexels.com/photos/378570/pexels-photo-378570.jpeg?auto=compress&cs=tinysrgb&w=100000', responseType: 'stream'})
+    axios({url: AppController.url, responseType: 'stream'})
         .then(response => response.data.pipe(sharp()))
         .then(source => source.resize(AppController.side, AppController.side, {fit: sharp.fit.outside}))
+        .then(file => file.pipe(response))
+  }
+
+  @Get("/contain/cubic")
+  containCubic(@Res() response): void {
+    axios({url: AppController.url, responseType: 'stream'})
+        .then(response => response.data.pipe(sharp()))
+        .then(source => source.resize(AppController.side, AppController.side, {
+          fit: sharp.fit.contain,
+          kernel: sharp.kernel.cubic
+        }))
+        .then(file => file.pipe(response))
+  }
+
+  @Get("/contain/nearest")
+  containNearest(@Res() response): void {
+    axios({url: AppController.url, responseType: 'stream'})
+        .then(response => response.data.pipe(sharp()))
+        .then(source => source.resize(AppController.side, AppController.side, {
+          fit: sharp.fit.contain,
+          kernel: sharp.kernel.nearest
+        }))
+        .then(file => file.pipe(response))
+  }
+
+  @Get("/contain/mitchell")
+  containMitchell(@Res() response): void {
+    axios({url: AppController.url, responseType: 'stream'})
+        .then(response => response.data.pipe(sharp()))
+        .then(source => source.resize(AppController.side, AppController.side, {
+          fit: sharp.fit.contain,
+          kernel: sharp.kernel.mitchell
+        }))
+        .then(file => file.pipe(response))
+  }
+
+  @Get("/contain/lanczos2")
+  containLanczos2(@Res() response): void {
+    axios({url: AppController.url, responseType: 'stream'})
+        .then(response => response.data.pipe(sharp()))
+        .then(source => source.resize(AppController.side, AppController.side, {
+          fit: sharp.fit.contain,
+          kernel: sharp.kernel.lanczos2
+        }))
         .then(file => file.pipe(response))
   }
 }
