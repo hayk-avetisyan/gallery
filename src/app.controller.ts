e9ccd3d432ffa972f1f5ede02777d10c7041acdf
Hayk Avetisyan <hayk.avetisyan@synisys.com>
2023-11-27 16:49:14 +0400

    Sample endpoints

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,50 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {Controller, Get, Res} from '@nestjs/common';
+import * as sharp from 'sharp';
+import axios from 'axios';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  
+  private static readonly side: number = 50;
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get("/contain")
+  contain(@Res() response): void {
+    axios({url: 'https://images.pexels.com/photos/378570/pexels-photo-378570.jpeg?auto=compress&cs=tinysrgb&w=100000', responseType: 'stream'})
+        .then(response => response.data.pipe(sharp()))
+        .then(source => source.resize(AppController.side, AppController.side, {fit: sharp.fit.contain}))
+        .then(file => file.pipe(response))
+  }
+
+  @Get("/fill")
+  fill(@Res() response): void {
+    axios({url: 'https://images.pexels.com/photos/378570/pexels-photo-378570.jpeg?auto=compress&cs=tinysrgb&w=100000', responseType: 'stream'})
+        .then(response => response.data.pipe(sharp()))
+        .then(source => source.resize(AppController.side, AppController.side, {fit: sharp.fit.fill}))
+        .then(file => file.pipe(response))
+  }
+
+
+  @Get("/cover")
+  cover(@Res() response): void {
+    axios({url: 'https://images.pexels.com/photos/378570/pexels-photo-378570.jpeg?auto=compress&cs=tinysrgb&w=100000', responseType: 'stream'})
+        .then(response => response.data.pipe(sharp()))
+        .then(source => source.resize(AppController.side, AppController.side, {fit: sharp.fit.cover}))
+        .then(file => file.pipe(response))
+  }
+
+  @Get("/inside")
+  inside(@Res() response): void {
+    axios({url: 'https://images.pexels.com/photos/378570/pexels-photo-378570.jpeg?auto=compress&cs=tinysrgb&w=100000', responseType: 'stream'})
+        .then(response => response.data.pipe(sharp()))
+        .then(source => source.resize(AppController.side, AppController.side, {fit: sharp.fit.inside}))
+        .then(file => file.pipe(response))
+  }
+
+  @Get("/outside")
+  outside(@Res() response): void {
+    axios({url: 'https://images.pexels.com/photos/378570/pexels-photo-378570.jpeg?auto=compress&cs=tinysrgb&w=100000', responseType: 'stream'})
+        .then(response => response.data.pipe(sharp()))
+        .then(source => source.resize(AppController.side, AppController.side, {fit: sharp.fit.outside}))
+        .then(file => file.pipe(response))
   }
 }
